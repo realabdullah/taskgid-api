0fb6955199f7206206d30929776b9dd10682b2d4
realabdullah <abdulodesanmi@gmail.com>
2026-05-03 12:35:10 +0100

    feat: move accept and decline invite routes before authentication middleware

--- src/routes/inviteRoutes.js
+++ src/routes/inviteRoutes.js
@@ -6,12 +6,13 @@ import {validateInviteInput} from '../middleware/validationMiddleware.js';
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
+router.post('/accept', acceptInvite);
+router.post('/decline', declineInvite);
+
 router.use(authMiddleware);
 
 router.get('/pending', getPendingInvitations);
 router.post('/', validateInviteInput, inviteUser);
 router.post('/bulk', inviteUsersBulk);
-router.post('/accept', acceptInvite);
-router.post('/decline', declineInvite);
 
 export default router;
